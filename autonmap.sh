1d13a14c9ab0d93c73bf89959ad7937b95a370ab
Fatake <pace_lozano@hotmail.com>
2023-11-09 12:45:00 -0500

    unix

--- autonmap.sh
+++ autonmap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 greenColour="\e[0;32m\033[1m";export greenColour;
 redColour="\e[0;31m\033[1m";export redColour;
 blueColour="\e[0;34m\033[1m";export blueColour;
@@ -9,27 +10,24 @@ turquoiseColour="\e[0;36m\033[1m";export turquoiseColour;
 grayColour="\e[0;37m\033[1m";export grayColour;
 endColour="\033[0m\e[0m";export endColour;
 
-function log_error() {
+log_error() {
   printf "[${redColour}✘${endColour}] $@\n"
 }
-
-function log_warning() {
+log_warning() {
   printf "[${yellowColour}⚠${endColour}] $@\n"
 }
-
-function log_ok() {
+log_ok() {
   printf "[${greenColour}✓${endColour}] $@\n"
 }
-
-function log_info() {
+log_info() {
   printf "[${lightblueColour}i${endColour}] $@\n"
 }
 
-function run_cmd () {
+run_cmd () {
   /bin/bash -c "$1" 2>/dev/null
 }
 
-function chekRoot(){
+chekRoot(){
   # Check if root launch
   if [ "$EUID" -ne 0 ]; then 
     log_error "Please run as root"
